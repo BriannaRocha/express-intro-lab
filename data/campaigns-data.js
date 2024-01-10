7fc251f682542f58f30efbd5150e3fddd2958b42
BriannaRocha <rochabrianna17@gmail.com>
2024-01-10 03:12:08 -0500

    add redirect

--- data/campaigns-data.js
+++ data/campaigns-data.js
@@ -1,11 +1,11 @@
 const campaigns = [
-  {text: 'Halo: Combat Evolved ', done: true, _id: 125223},
-  {text: 'Halo 2', done: true, _id: 127904},
-  {text: 'Halo 3', done: true, _id: 139608},
-  {text: 'Halo Reach', done: true, _id: 148511},
-  {text: 'Halo 4', done: true, _id: 110572},
-  {text: 'Halo 5: Guardians', done: false, _id: 459812},
-  {text: 'Halo Infinite', done: false, _id: 343343},
+  {text: 'Halo: Combat Evolved ', complete: true, _id: 125223},
+  {text: 'Halo 2', complete: true, _id: 127904},
+  {text: 'Halo 3', complete: true, _id: 139608},
+  {text: 'Halo Reach', complete: true, _id: 148511},
+  {text: 'Halo 4', complete: true, _id: 110572},
+  {text: 'Halo 5: Guardians', complete: false, _id: 459812},
+  {text: 'Halo Infinite', complete: false, _id: 343343},
 ]
 
 export { 
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs')
 
 // Mount routes
 app.get('/', function(req, res) {
-  res.send('<h1>Hello Express</h1>')
+  res.redirect('/home')
 })
 
 app.get('/home', function(req, res) {
