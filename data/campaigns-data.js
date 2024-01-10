e9bc24835fea8c47ad513b165e14640158405d90
BriannaRocha <rochabrianna17@gmail.com>
2024-01-10 02:28:42 -0500

    inital set up & create data.js file

---
+++ data/campaigns-data.js
@@ -0,0 +1,13 @@
+const campaigns = [
+  {text: 'Halo: Combat Evolved ', done: true, _id: 125223},
+  {text: 'Halo 2', done: true, _id: 127904},
+  {text: 'Halo 3', done: true, _id: 139608},
+  {text: 'Halo Reach', done: true, _id: 148511},
+  {text: 'Halo 4', done: true, _id: 110572},
+  {text: 'Halo 5: Guardians', done: false, _id: 459812},
+  {text: 'Halo Infinite', done: false, _id: 343343},
+]
+
+export { 
+	campaigns
+}
